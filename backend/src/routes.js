ad487d7fb7add7d3d63352f53c092b42b77e4794
richardjlv <richardjunio39@gmail.com>
2020-03-13 14:02:48 -0300

    Adding backend

---
+++ backend/src/routes.js
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+import multer from 'multer';
+
+// import FileController from './app/controllers/FileController';
+// import multerConfig from './config/multer';
+
+const routes = new Router();
+// const upload = multer(multerConfig);
+
+// routes.post('/files', upload.single('file'), FileController.store);
+
+export default routes;
